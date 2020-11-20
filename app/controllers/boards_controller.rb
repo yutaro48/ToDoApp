4c29f258cc82d1a3ed3134823f4ae176d20c42c7
Horino Yutaro <suzy.city16@gmail.com>
2020-11-20 15:39:54 +0900

    createアクションを使ったDBへの新規登録機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,4 +12,18 @@ class BoardsController < ApplicationController
         @board = Board.new
     end
 
+    def create
+        @board = Board.new(article_params)
+        if @board.save
+            redirect_to board_path(@board)
+        else
+            render :new
+        end
+    end
+
+    private
+    def board_params
+        params.require(:board).permit(:name, :description)
+    end
+
 end
