2852f87b2116e814d0995b4ee6c4861bf7515e77
Horino Yutaro <suzy.city16@gmail.com>
2020-11-21 10:22:47 +0900

    editアクションを実装して、一覧表示ページにリンクを作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -22,6 +22,10 @@ class BoardsController < ApplicationController
         end
     end
 
+    def edit
+        @board = Board.find(params[:id])
+    end
+
     private
     def board_params
         params.require(:board).permit(:name, :description)
