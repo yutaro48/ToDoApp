ea74a3b449fe6bdc18f5d010314d2ddfc4f00e88
Horino Yutaro <suzy.city16@gmail.com>
2020-11-21 20:17:59 +0900

    migrationファイルの修正

--- db/migrate/20201117041911_create_boards.rb
+++ db/migrate/20201117041911_create_boards.rb
@@ -1,6 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[6.0]
   def change
     create_table :boards do |t|
+      t.references :user, null: false
       t.string :name, null: false
       t.text :description, null: false
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_042413) do
+ActiveRecord::Schema.define(version: 2020_11_17_041911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
