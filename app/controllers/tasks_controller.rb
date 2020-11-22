0828846fe84d76f115ef304b2ec7e3c55a959950
Horino Yutaro <suzy.city16@gmail.com>
2020-11-22 21:48:44 +0900

    showアクションの実装（コントローラの整備）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
     def new
         board = Board.find(params[:board_id])
         @task = board.tasks.build
