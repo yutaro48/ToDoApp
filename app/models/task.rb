8907bef133d0fe88ac9f833dc2f769b0adfd3fa6
Horino Yutaro <suzy.city16@gmail.com>
2020-11-23 12:50:33 +0900

    モデルとコントローラの削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,16 +9,13 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  board_id    :bigint           not null
-#  user_id     :bigint           not null
 #
 # Indexes
 #
 #  index_tasks_on_board_id  (board_id)
-#  index_tasks_on_user_id   (user_id)
 #
 class Task < ApplicationRecord
     belongs_to :board
-    belongs_to :user
 
     validates :name, presence: true
     validates :description, presence: true
