3657cb764e8062a4cc5b84a9446a3feeb7ff702e
Horino Yutaro <suzy.city16@gmail.com>
2020-11-23 12:37:12 +0900

    モデルとコントローラの修正

--- db/migrate/20201122105524_create_tasks.rb
+++ db/migrate/20201122105524_create_tasks.rb
@@ -2,6 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.references :board, null: false
+      t.references :user, null: false
       t.string :name, null: false
       t.text :description, null: false
       t.date :date, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,14 @@ ActiveRecord::Schema.define(version: 2020_11_22_105524) do
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "board_id", null: false
+    t.bigint "user_id", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_tasks_on_board_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
